5bb48bd9f9356111ac07b9bc2a26f154c54feceb
collins52 <collinsomonievusa@gmail.com>
2026-01-20 16:39:41 +0100

    Add role input

--- addStaff/addStaff.js
+++ addStaff/addStaff.js
@@ -1,1 +1,12 @@
 // Add Staff page scripts
+// In addStaff.js
+const form = document.querySelector('.staff-form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const userName = form.querySelector('input[type="text"]').value;
+  const staffID = form.querySelector('.staff-id-input').value;
+  const role = form.querySelector('.staff-role').value;
+
+  console.log({ userName, staffID, role });
+  // You can now send this data to Firebase
+});
